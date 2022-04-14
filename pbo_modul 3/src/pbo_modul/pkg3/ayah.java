8437c2402511fa61e4f6bd114e256986b1df9521
F4dhil54 <103545327+F4dhil54@users.noreply.github.com>
2022-04-14 07:01:47 +0700

    Add files via upload

---
+++ pbo_modul 3/src/pbo_modul/pkg3/ayah.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_modul.pkg3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ayah {
+
+   
+    String Sifat= "Penyabar";
+    double TB= 173.1;
+    
+    void hobbi(){
+        System.out.println("Hobi nya adalah Ngo-piding=Ngopi dan Ngoding");
+    }
+}
+
+
---
+++ src/pratikum/hasil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratikum;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class hasil {
+     public static void main(String[]args){
+        Identitas fadhil=new Identitas();
+        fadhil.setnama("fadhil");
+        fadhil.setjenisK("Laki-laki");
+        fadhil.cetak();
+    }
+}
+
